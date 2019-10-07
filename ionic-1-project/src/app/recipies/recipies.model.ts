2bfd308992ba75a45f29dfab77d2d3edfa8a5ad1
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-10-07 18:24:17 +0500

    recipies done

---
+++ ionic-1-project/src/app/recipies/recipies.model.ts
@@ -0,0 +1,6 @@
+export interface Recipie {
+    id: string;
+    title: string;
+    imageURL: string;
+    ingredients: string[];
+}
--- ionic-1-project/src/app/recipies/recipies.page.ts
+++ ionic-1-project/src/app/recipies/recipies.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipie} from './recipies.model';
 
 @Component({
     selector: 'app-recipies',
@@ -6,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./recipies.page.scss'],
 })
 export class RecipiesPage implements OnInit {
+
+    recipies: Recipie[] = [
+        {
+            id: '1',
+            title: 'Aloo',
+            imageURL: 'https://www.coloradopotato.org/wp-content/themes/weboenhance/images/food-potato.png',
+            ingredients: [
+                'Mirch',
+                'Panni',
+                'Aag'
+            ]
+        },
+        {
+            id: '2',
+            title: 'Baryani',
+            imageURL: 'http://www.hansonfoods.com.pk/kj-content/uploads/2013/10/muton-baryani.jpg',
+            ingredients: [
+                'Chawal',
+                'Chicken',
+                'Alo'
+            ]
+        },
+
+    ];
 
     constructor() { }
 
