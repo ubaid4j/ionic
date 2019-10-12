0bf597bb09d45f389044eb79160e8ebeeb58a0a4
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-10-13 01:29:06 +0500

    routing done

---
+++ booking-ionic/src/app/places/places.routing.module.ts
@@ -0,0 +1,31 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PlacesPage} from './places.page';
+import {path} from '@angular-devkit/core';
+
+const routes: Routes = [
+    {path: '', redirectTo: '/places/tabs/discover', pathMatch: 'full'},
+    {path: 'tabs',
+    component: PlacesPage,
+    children: [
+        {path: '', redirectTo: '/places/tabs/discover', pathMatch: 'full'},
+        {path: 'discover',
+        children: [
+            {path: '', loadChildren: './discover/discover.module#DiscoverPageModule'},
+            {path: ':placeId', loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'}
+        ]},
+        {path: 'offers',
+        children: [
+            {path: '', loadChildren: './offers/offers.module#OffersPageModule'},
+            {path: 'new', loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule'},
+            {path: 'edit/:placeId', loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'},
+            {path: ':placeId', loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'}
+        ]}
+    ]}
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class PlacesRoutingModule {}
