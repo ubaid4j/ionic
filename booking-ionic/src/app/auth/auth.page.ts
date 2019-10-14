9f833edf11bf17221117f7e3dd6c33b625cd9744
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-10-14 09:34:48 +0500

    added

--- booking-ionic/src/app/auth/auth.page.ts
+++ booking-ionic/src/app/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../service/auth/auth.service';
 import {Router} from '@angular/router';
+import {LoadingController} from '@ionic/angular';
 
 @Component({
     selector: 'app-auth',
@@ -8,15 +9,30 @@ import {Router} from '@angular/router';
     styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-
+    private isLoadingF = false;
     constructor(private authService: AuthService,
-                private router: Router) { }
-
+                private router: Router,
+                private loadingCont: LoadingController) {
+    }
     ngOnInit() {
     }
 
-    login() {
-        this.authService.login();
-        this.router.navigate(['']).then(r => {});
+    public login() {
+        this.loadingCont.create({
+           keyboardClose: true,
+           message: 'Logging In....'
+        }).then(loadingElement => {
+            loadingElement.present().then();
+            setTimeout(() => {
+                this.isLoadingF = false;
+                this.authService.login();
+                loadingElement.dismiss().then();
+                this.router.navigate(['']).then();
+            }, 1000);
+        });
+        this.isLoadingF = true;
+    }
+    public isLoading(): boolean {
+        return this.isLoadingF;
     }
 }
